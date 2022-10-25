63a6074f4bb77fe37e6fa9e581ad66bcb15cd1ac
DANISHALI-08 <116626273+DANISHALI-08@users.noreply.github.com>
2022-10-25 12:58:11 +0530

    Add files via upload

---
+++ INEURON.java
@@ -0,0 +1,115 @@
+package Pattern;
+
+public class INEURON {
+	
+	public static void main(String[] args) {
+		int n=7;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+//I
+				if(j==0)
+					 {
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");	
+				}
+			}
+			for(int j=0;j<n;j++) {
+ //N
+				if(j==0||j==n-1||i==j)
+					 {
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");	
+				}
+			}
+			for(int j=0;j<n;j++) {
+//E
+				if(j==0||(i==n-1)||(i==(n-1)/2&&j<(n-1))
+				||(i==0))
+					 {
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");	
+				}
+			}
+			for(int j=0;j<n;j++) {
+				//U
+				if(j==0&&i!=n-1||(j==n-1&&i!=n-1)||i==n-1&&j!=0&&j<n-1)
+				
+					 {
+						System.out.print("*");
+						}
+						else
+						{
+							System.out.print(" ");	
+								}
+							}
+			for(int j=0;j<n;j++) {
+				//R
+				if(j==0||(i==0&&j!=n-1&&j!=0)||
+				i==(n-1)/2&&j!=n-1&&j!=0||
+				j==n-1&&i<(n-1)/2&&i!=0||i-j==(n-1)/2)
+				
+					 {
+						System.out.print("*");
+						}
+						else
+						{
+							System.out.print(" ");	
+								}
+							}
+			for(int j=0;j<n;j++) {
+				//O
+				if(j==0&&i!=0&&i!=n-1||(j==n-1&&i<n-1&&i!=0)
+				||(i==0&&j>0&&j<n-1)||(i==n-1&&j>0&&j<n-1))
+				
+					 {
+						System.out.print("*");
+						}
+						else
+						{
+							System.out.print(" ");	
+								}
+							}
+			for(int j=0;j<n;j++) {
+				 //N
+				if(j==0||j==n-1||i==j)
+				{
+					System.out.print("*");
+					}
+						else
+						{
+							System.out.print(" ");	
+								}
+						}
+				
+			System.out.println("");
+		}
+	}
+
+}
+
+	
+//	int n=4;
+//	for(int i=0;i<n;i++) {
+//		for(int j=0;j<n;j++) {
+//			if(i==0||i==3||j==0||j==3) {
+//		System.out.print("*");
+//		}
+//		else 
+//		{
+//			System.out.print(" ");
+//		}
+//	}
+//	
+//	System.out.println();	
+//}
+//
+//}
---
+++ V_Shape.java
@@ -0,0 +1,26 @@
+package Pattern;
+
+public class V_Shape {
+	public static void main(String[] args) {
+		int n=14;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+
+				if(i==n-1||i>(n-1)/2&&j==0||(j==(n-1)&&i>(n-1)/2)
+				||i-j>=(n-1)/2||i+j>=(n-1)+((n-1)/2))
+					 {
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");	
+				}
+			}
+					
+			System.out.println("");
+		}
+	}
+
+}
+
+
